b672d9bb5f306fe3917b5b53ee20f2c09e551f5e
Srilekha V. V <srilekha.v@tadigital.com>
2020-02-11 13:38:59 +0530

    Initial Commit

---
+++ assets/js/tours.js
@@ -0,0 +1,5 @@
+$(function(){
+    var tours= [{'time':'May 25th-june 01st','city':'Mauritious','price':'From $1450','seemore':'SEE MORE...'},
+        {'time':'May 25th-june 01st','city':'Mauritious','price':'From $1450','seemore':'SEE MORE...'},
+        {'time':'May 25th-june 01st','city':'Mauritious','price':'From $1450','seemore':'SEE MORE...'}] 
+});
